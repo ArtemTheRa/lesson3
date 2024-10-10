b5bfabb30e1a36d71a1a42229406b96516536dca
ArtemTheRa <artyomthera@gmail.com>
2024-10-11 00:34:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+Number_of_homework_completed = 12.0
+Number_of_hours_spent = 1.5
+Course_name = "Python"
+print("Курс:", Course_name, ",всего задач:", int(Number_of_homework_completed), ",затрачено часов:", Number_of_hours_spent, "среднее время выполнения", Number_of_hours_spent / Number_of_homework_completed, "часа")
